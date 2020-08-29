cea36595cb040f9f6508563663000de2a2433f44
Olivia Chang <olivia.chang@limelighthealth.com>
2020-08-29 09:56:42 -0700

    remove eleventy

--- README.md
+++
@@ -1,3 +0,0 @@
-# Kindle Picture Frame Website
-Automatically redirects to a random image from `/src/assets/images`.
-Resizes images to be 600x800.
---
+++ build.js
@@ -0,0 +1,24 @@
+const fs = require('fs-extra')
+
+const path = require('path');
+const fg = require("fast-glob");
+const images_array = fg.sync(["images/*.{jpg,jpeg,png}", "!**/_site"]);
+let images_string = ''
+
+const SITE_URL = "https://kpf.netlify.app"
+
+images_array.forEach((url) => {
+    url = path.join(SITE_URL, url);
+    images_string += url + ', '
+});
+
+fs.mkdir('_dist', { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.copy('images', '_dist/images')
+
+fs.writeFile('_dist/index.html', images_string, 'utf8', (err) => {
+  console.log(err)
+})
+
